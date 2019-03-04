5c337a64e5295102e79f72b9a5149affe123b830
Aleksluciano <aleksluciano@gmail.com>
2019-03-04 14:38:14 -0300

    arvore update

--- src/app/components/arvores/arvores.component.ts
+++ src/app/components/arvores/arvores.component.ts
@@ -1,16 +1,14 @@
 import { Voluntario } from './../../models/voluntario.model';
 import { Component, OnInit } from '@angular/core';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { VoluntarioService } from 'src/app/services/voluntario.service';
-import {TooltipPosition} from '@angular/material';
+import { TooltipPosition } from '@angular/material';
 import { FormControl } from '@angular/forms';
 interface ArvoreNode {
   name: string;
   children?: [];
 }
-
-
 
 @Component({
   selector: 'app-arvores',
@@ -18,83 +16,136 @@ interface ArvoreNode {
   styleUrls: ['./arvores.component.scss']
 })
 export class ArvoresComponent implements OnInit {
+  voluntariosIndependentes = [];
+  voluntariosComDependencia = [];
 
-  voluntarios = [];
-  voluntarioRef = [];
 
   treeControl = new NestedTreeControl<ArvoreNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<ArvoreNode>();
 
-
   total = 0;
   tot = 0;
 
-  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
+  positionOptions: TooltipPosition[] = [
+    'after',
+    'before',
+    'above',
+    'below',
+    'left',
+    'right'
+  ];
   position = new FormControl(this.positionOptions[0]);
 
-  constructor(private voluntarioService: VoluntarioService) {
-  }
+  constructor(private voluntarioService: VoluntarioService) {}
 
-
-   ngOnInit() {
-
+  ngOnInit() {
     this.voluntarioService.get().subscribe(data => {
       const vol = [...data];
-      const todo = [];
+      const todosVoluntarios = [];
 
       this.total = vol.length;
 
+      this.montaListaCompleta(vol, todosVoluntarios);
+      this.montaListaComDependentes(todosVoluntarios);
 
+      this.voluntariosIndependentes = todosVoluntarios.filter(
+        a => !a.dependente
+      );
+      this.acheoTotal(this.voluntariosIndependentes);
 
-      vol.forEach(a => {
-       // if(a.dependente)
-       let obj = { id: a.id, name: a.nome, children: [], dependente: a.dependente, nomeDependente: a.nomeDependente, congregacao: a.congregacao}
-       todo.push(obj);
-       if (a.dependente) {
-       this.voluntarioRef.push(obj);
-       }
-
-      });
-
-      todo.forEach(a => {
-// tslint:disable-next-line: triple-equals
-const child = this.voluntarioRef.filter(b => b.nomeDependente == a['id'] );
-if (child.length > 0) {
-  child.forEach(c => a['children'].push(c));
-}
-
-          });
-
-
-     this.voluntarios = todo.filter(a => !a.dependente);
-     this.acheoTotal(this.voluntarios)
-     console.log(this.voluntarios)
-     this.dataSource.data = [...this.voluntarios];
-
-
-
-
+      this.dataSource.data = [...this.voluntariosIndependentes];
 
 
     });
   }
 
-  hasChild = (_: number, node: ArvoreNode) => !!node.children && node.children.length > 0;
 
 
-  acheoTotal(a) {
-
-    a.forEach(b => {
 
 
-    if (b.children) {
-    this.acheoTotal(b.children);
-      console.log(b.children.length);
-    }
+  hasChild = (_: number, node: ArvoreNode) =>
+    !!node.children && node.children.length > 0
 
-    });
+  acheoTotal(a, x = 1) {
 
 
+    a.forEach((b,i) => {
+
+      if (b.children.length) {
+        x += b.total;
+
+        let val = this.acheoTotal(b.children);
+
+        b.quant = val;
+
+      }else{
+        x++;
       }
+      
+       });
 
+
+
+ return x;
+  }
+  // acheoTotal(a) {
+
+  //   let x = 0;
+
+  //       a.forEach((b,i) => {
+  //   console.log(b.name, b.quant)
+  //   if(i === 0)x = 1;
+  //         if (b.children.length) {
+  //           x += b.total;
+
+  //           let val = this.acheoTotal(b.children);
+
+  //           b.quant = val;
+  //           console.log("222",b.name, b.quant,x)
+  //         }else{
+  //           x++;
+  //         }
+  //          });
+
+
+
+  //    return x;
+  //     }
+
+  montaListaCompleta(vol, todosVoluntarios) {
+    vol.forEach(a => {
+      // if(a.dependente)
+      const obj = {
+        id: a.id,
+        name: a.nome,
+        children: [],
+        dependente: a.dependente,
+        nomeDependente: a.nomeDependente,
+        congregacao: a.congregacao,
+        quant: 1,
+        total: 1
+      };
+      todosVoluntarios.push(obj);
+      if (a.dependente) {
+        this.voluntariosComDependencia.push(obj);
+      }
+    });
+  }
+
+  montaListaComDependentes(todosVoluntarios) {
+    todosVoluntarios.forEach(a => {
+      // tslint:disable-next-line: triple-equals
+      const child = this.voluntariosComDependencia.filter(
+        b => b.nomeDependente === a['id']
+      );
+      if (child.length > 0) {
+        child.forEach(c => {
+
+          a.total += c.total;
+          a['children'].push(c);
+
+        });
+      }
+    });
+  }
 }
